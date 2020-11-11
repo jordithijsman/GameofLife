16f0a2bae44cbe068a74edabc7af72fff565528e
jordithijsman <45132169+jordithijsman@users.noreply.github.com>
2020-11-11 15:01:31 +0100

    Update World.java

--- Project/Klassen/World.java
+++ Project/Klassen/World.java
@@ -25,11 +25,11 @@ public class World {
     public World(int width, int height) {
         this.breedte = width;
         this.hoogte = height;
-        for (int i = 0; i < hoogte; i++) {
-            yMap.put(i, new Vakje());
-        }
         for (int i = 0; i < breedte; i++) {
-            puntenMap.put(i, yMap);
+            puntenMap.put(i, new HashMap<>());
+            for(int k = 0; k < hoogte; k++){
+                puntenMap.get(i).put(k, new Vakje());
+            }
         }
     }
 
@@ -39,6 +39,8 @@ public class World {
         }else{
             puntenMap.get(x).get(y).setAlive(true);
         }
+        Buren(x,y);
+        System.out.println(puntenMap.get(1).get(1).getBuren());
     }
 
     public void saveToFile(final File file) throws IOException {
@@ -57,6 +59,38 @@ public class World {
 
     public boolean isAliveAt(int x, int y) {
         return puntenMap.get(x).get(y).getAlive();
+    }
+
+    private void Buren(int x, int y){
+        char teken;
+        if(puntenMap.get(x).get(y).getAlive()){
+            teken = '+';
+        }else teken = '-';
+
+        if(x!=0){
+            puntenMap.get(x-1).get(y).wijzigAantaBuren(teken);
+            if(y!= hoogte-1){
+                puntenMap.get(x-1).get(y+1).wijzigAantaBuren(teken);
+            }
+            if(y!=0){
+                puntenMap.get(x-1).get(y-1).wijzigAantaBuren(teken);
+            }
+        }
+        if(x!= breedte-1){
+            puntenMap.get(x+1).get(y).wijzigAantaBuren(teken);
+            if(y!=hoogte-1){
+                puntenMap.get(x+1).get(y+1).wijzigAantaBuren(teken);
+            }
+            if(y!=0){
+                puntenMap.get(x+1).get(y-1).wijzigAantaBuren(teken);
+            }
+        }
+        if(y!=0){
+            puntenMap.get(x).get(y-1).wijzigAantaBuren(teken);
+        }
+        if(y!=hoogte-1){
+            puntenMap.get(x).get(y+1).wijzigAantaBuren(teken);
+        }
     }
 
     public void randomCells() { // vult hele lijn??
